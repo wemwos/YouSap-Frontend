4a608cb5fe639ead279b54fccd0abac53fbd51f7
Kainoveda <Kainoveda@gmail.com>
2024-05-14 00:59:52 +0800

    Added Upload Modal

--- src/components/UploadModal.jsx
+++ src/components/UploadModal.jsx
@@ -6,7 +6,8 @@ import styles from '../css/upload-modal.module.css';
 const UploadModal = ({ show, handleClose, handleUpload }) => {
   const [content, setContent] = useState({
     type: 'photo',
-    file: null
+    file: null,
+    text: '' // Added text field
   });
 
   const handleContentChange = (type) => {
@@ -20,6 +21,13 @@ const UploadModal = ({ show, handleClose, handleUpload }) => {
     setContent(prevContent => ({
       ...prevContent,
       file: e.target.files[0]
+    }));
+  };
+
+  const handleTextChange = (e) => {
+    setContent(prevContent => ({
+      ...prevContent,
+      text: e.target.value
     }));
   };
 
@@ -37,28 +45,28 @@ const UploadModal = ({ show, handleClose, handleUpload }) => {
       <Modal.Body className={styles.uploadModal_body}>
         <Form onSubmit={handleSubmit}>
           <Form.Group controlId="contentType">
-            <Form.Label>Content Type</Form.Label>
             <div>
-              <Button variant="light" onClick={() => handleContentChange('photo')} className={`${styles.uploadModal_button} mr-2`}>
-                <FaCamera /> Photo
+              <Button onClick={() => handleContentChange('photo')} className={`${styles.uploadModal_button} mr-2`}>
+                <FaCamera /> 
               </Button>
-              <Button variant="light" onClick={() => handleContentChange('video')} className={styles.uploadModal_button}>
-                <FaVideo /> Video
+              <Button  onClick={() => handleContentChange('video')} className={styles.uploadModal_button}>
+                <FaVideo /> 
               </Button>
             </div>
           </Form.Group>
           {content.type === 'photo' && (
             <Form.Group controlId="photo">
-              <Form.Label>Upload Photo</Form.Label>
               <Form.Control type="file" accept="image/*" onChange={handleFileChange} className={styles.uploadModal_input} />
             </Form.Group>
           )}
           {content.type === 'video' && (
             <Form.Group controlId="video">
-              <Form.Label>Upload Video</Form.Label>
               <Form.Control type="file" accept="video/*" onChange={handleFileChange} className={styles.uploadModal_input} />
             </Form.Group>
           )}
+          <Form.Group controlId="text">
+            <Form.Control as="textarea" placeholder='What are you up to today?'  rows={3} value={content.text} onChange={handleTextChange} className={styles.uploadModal_input} />
+          </Form.Group>
           <Button variant="primary" type="submit" className={styles.uploadModal_uploadButton}>
             Upload
           </Button>
